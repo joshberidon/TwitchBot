31f60fec0b1c921cc064ec98ef11d9438390d078
Josh Beridon <jberidon@gmail.com>
2016-08-06 12:57:48 -0500

    initial commit

---
+++ src/main/java/com/twitchbot/Main.java
@@ -0,0 +1,11 @@
+package com.twitchbot;
+
+/**
+ * Created by JoshBeridon on 8/6/16.
+ */
+public class Main {
+    public static void main (String [] args){
+        System.out.print("Hello world!");
+
+    }
+}
